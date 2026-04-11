1776bf24e0569bc7074d0206d38c811c967ca877
Nathan Marty <nathanm32712@gmail.com>
2026-04-10 21:53:51 -0500

    Add files via upload

---
+++ tabs/tabs.js
@@ -0,0 +1,47 @@
+window.launchdeckTabs = [
+
+    // ---------------- INFO ----------------
+    // Visible to everyone
+    "info/welcome.html|guest",
+    "info/changelog.html|guest",
+
+    // Login page only for guests (hidden once logged in)
+    "info/login.html|guest",
+
+    // Admin panel only for admin + owner
+    "info/admin.html|admin",
+
+
+    // ---------------- TOOLS ----------------
+    // Tools require login
+    "tools/calc.html|user",
+    "tools/clock.html|user",
+    "tools/mapquiz.html|user",
+
+
+    // ---------------- GAMES ----------------
+    // ALL games require login
+    "games/clicker.html|user",
+    "games/towerdefence.html|user",
+    "games/reaction.html|user",
+    "games/memory.html|user",
+    "games/2048.html|user",
+    "games/snake.html|user",
+    "games/breakout.html|user",
+
+
+    // ---------------- CASINO ----------------
+    // ALL casino games require login
+    "casino/limbo.html|user",
+    "casino/blackjack.html|user",
+    "casino/slots.html|user",
+    "casino/plinko.html|user",
+    "casino/crash.html|user",
+    "casino/wheel.html|user",
+    "casino/mines.html|user",
+    "casino/hilo.html|user",
+    "casino/keno.html|user",
+    "casino/coinflip.html|user",
+    "casino/diceduel.html|user",
+    "casino/double.html|user"
+];
